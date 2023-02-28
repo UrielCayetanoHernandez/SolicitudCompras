fadf9af5f7ae4ea2470591dacd41b2df60afdce4
UrielCayetanoHernandez <al221510804@gmail.com>
2023-02-28 15:06:35 -0600

    Se agrega las primeras migraciones

---
+++ app/Models/usuario.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class usuario extends Model
+{
+    use HasFactory;
+
+    public function role()
+    {
+        return  $this->belongsTo(rol::class, 'id_rol','id_roles');
+    }
+}
---
+++ database/seeders/RolSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class RolSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('roles')->insert([
+            'nom_rol' => Str::random(8),
+            'descri_rol' => Str::random(8)
+        ]);
+    }
+}
